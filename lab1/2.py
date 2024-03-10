78e2c0d1923d789cce34ca21dee69a48ec347082
met4000 <nathsharwor@hotmail.com>
2024-03-10 18:32:36 +0800

    lab 1 exp 2

---
+++ lab1/2.py
@@ -0,0 +1,72 @@
+from math import inf
+from typing import Optional
+from eye import * # type: ignore
+
+def VWStop():
+    VWSetSpeed(0, 0)
+
+def turn_to_closest(*, angular_speed = 60, encoder = 1):
+    ENCODERReset(encoder)
+    VWTurn(360, angular_speed)
+
+    min_dst = inf
+    min_dst_encoder = 0
+    while not VWDone():
+        front_distance = PSDGet(PSD_FRONT)
+        if front_distance < min_dst:
+            min_dst = front_distance
+            min_dst_encoder = ENCODERRead(encoder)
+    VWStop()
+    final_encoder = ENCODERRead(encoder)
+
+    target_angle: int = round(360 * (final_encoder - min_dst_encoder) / final_encoder % 360)
+    if target_angle > 180: target_angle = 360 - target_angle
+
+    VWTurn(target_angle, angular_speed); VWWait()
+
+def follow_wall_until_distance(stop_distance_mm: int, *, wall_distance_mm: Optional[int] = None, speed = 200, psd = PSD_RIGHT):
+    target_distance_mm = wall_distance_mm if wall_distance_mm else PSDGet(psd)
+
+    kp = 10
+    while PSDGet(PSD_FRONT) > stop_distance_mm:
+        side_distance_mm = PSDGet(psd)
+        error = target_distance_mm - side_distance_mm
+        VWSetSpeed(speed, error * kp)
+    VWStop()
+
+
+DISTANCE_FROM_WALL_MM = 150
+DISTANCE_BETWEEN_PASSES_MM = DISTANCE_FROM_WALL_MM * 2
+
+FORWARD_SPEED = 200
+FAST_TURN_SPEED = 90
+SCAN_TURN_SPEED = 60
+
+
+# go to nearest wall
+turn_to_closest(angular_speed=SCAN_TURN_SPEED)
+follow_wall_until_distance(DISTANCE_FROM_WALL_MM, speed=FORWARD_SPEED)
+
+# go to a corner
+VWTurn(90, FAST_TURN_SPEED); VWWait()
+follow_wall_until_distance(DISTANCE_FROM_WALL_MM, speed=FORWARD_SPEED)
+VWTurn(90, FAST_TURN_SPEED); VWWait()
+
+# 'lawnmower' pattern
+final_row = False
+flip_turn = False
+while not final_row:
+    follow_wall_until_distance(DISTANCE_FROM_WALL_MM, speed=FORWARD_SPEED)
+    VWTurn(90 * (-1 if flip_turn else 1), FAST_TURN_SPEED); VWWait()
+
+    VWStraight(DISTANCE_BETWEEN_PASSES_MM, FORWARD_SPEED)
+    while not VWDone():
+        if PSDGet(PSD_FRONT) < DISTANCE_FROM_WALL_MM:
+            final_row = True
+            break
+
+    VWTurn(90 * (-1 if flip_turn else 1), FAST_TURN_SPEED); VWWait()
+    flip_turn = not flip_turn
+
+# final row
+follow_wall_until_distance(DISTANCE_FROM_WALL_MM, speed=FORWARD_SPEED)
