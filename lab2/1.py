f892f24bb9c085aadd56f4e9b67083e7ab528387
met4000 <nathsharwor@hotmail.com>
2024-03-16 20:44:59 +0800

    implement pure pursuit

--- lab2/1.py
+++ lab2/1.py
@@ -15,51 +15,60 @@ def H4(u):
     return u**3 - u**2
 
 def SplineDrive(*, x: int, y: int, alpha: int):
-    n_steps = 10
+    lin_speed = 400
+    lookahead = 0.06
+    end_threshold = 0.06
+    kp = 1.6
 
     ax = 0
     ay = 0
     bx = x
     by = y
 
-    path_length = 2 * math.sqrt(x**2 + y**2)
-    v_ax = path_length
+    path_length = math.sqrt(x**2 + y**2)
+    v_k = 2
+    v_ax = path_length * v_k
     v_ay = 0
-    v_bx = path_length * math.cos(alpha)
-    v_by = path_length * math.sin(alpha)
+    v_bx = path_length * v_k * math.cos(alpha)
+    v_by = path_length * v_k * math.sin(alpha)
 
     VWSetPosition(x=0, y=0, phi=0)
 
-    path: list[tuple[float, float]] = []
-    for u in range(n_steps + 1):
-        u /= n_steps
-        path.append((
+    def spline(u: float) -> tuple[float, float]:
+        return (
             H1(u) * ax + H2(u) * bx + H3(u) * v_ax + H4(u) * v_bx,
             H1(u) * ay + H2(u) * by + H3(u) * v_ay + H4(u) * v_by
-        ))
+        )
     
-    for i in range(len(path)):
-        sp_x = path[i][0]
-        sp_y = path[i][1]
+    path_duration_ms = path_length / lin_speed * 1000
+    initial_ms = OSGetCount()
+    while True:
+        current_ms = OSGetCount() - initial_ms
+        current_u = current_ms / path_duration_ms
+        tracking_u = current_u + lookahead
 
-        current_x, current_y, current_phi = VWGetPosition()
+        tracking_x, tracking_y = spline(min(tracking_u, 1.0))
+        current_x, current_y, current_bearing = VWGetPosition()
 
-        err_x = sp_x - current_x
-        err_y = sp_y - current_y
-        target_phi = math.atan2(err_y, err_x) * 180 / math.pi
-        rotation = target_phi - current_phi
+        dx = tracking_x - current_x
+        dy = tracking_y - current_y
+        if tracking_u > 1.0 and math.sqrt(dx**2 + dy**2) / path_length < end_threshold:
+            break
 
-        rotation_radians = rotation / 180 * math.pi
-        ratio = math.sin(rotation_radians / 2)
-        radius = math.sqrt(err_x**2 + err_y**2) / 2 / ratio if ratio != 0 else 0
+        tracking_bearing = math.atan2(dy, dx) * 180 / math.pi
+        err_angle = tracking_bearing - current_bearing
 
-        distance = rotation_radians * radius
-        VWCurve(dist=round(distance), angle=round(rotation), lin_speed=500)
-        VWWait()
+        ang_speed = kp * err_angle
+        VWSetSpeed(lin_speed=lin_speed, ang_speed=round(ang_speed))
 
-# SplineDrive(x=1500, y=-1000, alpha=90)
+    VWStop()
 
-SplineDrive(x=1000, y=0, alpha=0)
+from eye import SIMSetRobot
+SIMSetRobot(0, 225, 210, 4, 0)
+
+SplineDrive(x=1000, y=1500, alpha=0)
+
+# SplineDrive(x=1000, y=0, alpha=0)
 # SplineDrive(x=0, y=1000, alpha=90)
 # SplineDrive(x=0, y=1000, alpha=0)
 # SplineDrive(x=-500, y=-10, alpha=0)
